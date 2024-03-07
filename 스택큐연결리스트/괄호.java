f611087fe56ce000c9fa18e7ae94ce424d1f5737
kwonjiyun <ziyun1612@ewhain.net>
2024-03-07 19:31:23 +0900

    스택 : 괄호(주석 추가)

--- 스택큐연결리스트/괄호.java
+++ 스택큐연결리스트/괄호.java
@@ -19,14 +19,14 @@ public class 괄호 {
             Deque<Character> openStack = new ArrayDeque<>();
             char[] input = br.readLine().toCharArray();
             for(char c : input){
-                if(c == '('){
+                if(c == '('){ // 여는 괄호라면 스택에 넣는다.
                     openStack.push(c);
-                }else{
-                   if(openStack.isEmpty()){
-                       flag = false;
+                }else{ // 닫는 괄호라면
+                   if(openStack.isEmpty()){ // 1) 닫는 괄호인데 stack이 비어있다면 괄호 문자열 조건에 맞지 않으므로 틀린 것이다.
+                       flag = false; // 괄호 문자열 조건이 아니라는 것을 표시
                        break;
                    }else{
-                       openStack.pop();
+                       openStack.pop(); // 2) 닫는 괄호인데 스택에 여는 괄호가 있다면 즉, 앞서 짝꿍인 여는 괄호가 있다는 의미이므로 조건 통과
                    }
                 }
             }
