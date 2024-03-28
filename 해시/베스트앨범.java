f208f86616a7b275b2e1769bea5b09f2cca773ee
kwonjiyun <ziyun1612@ewhain.net>
2024-03-28 18:43:25 +0900

    해시 : 베스트앨범

---
+++ 해시/베스트앨범.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class 베스트앨범 {
+    public int[] solution(String[] genres, int[] plays) {
+
+        Map<String, Info> dic = new HashMap<>();
+        PriorityQueue<Info> sortNum = new PriorityQueue<>();
+        for(int i = 0; i < genres.length; i++){
+            if(dic.keySet().contains(genres[i])){
+                Info getInfo = dic.get(genres[i]);
+                getInfo.total += plays[i];
+                getInfo.pq.offer(new Node(i, plays[i]));
+                dic.put(genres[i], getInfo);
+
+            }
+            else{
+                Info info = new Info(plays[i]);
+                info.pq.offer(new Node(i, plays[i]));
+                dic.put(genres[i], info);
+            }
+        }
+        for(String key : dic.keySet()){
+            sortNum.offer(dic.get(key));
+        }
+
+        List<Integer> answer = new ArrayList<>();
+
+        while(!sortNum.isEmpty()){
+            Info target = sortNum.poll();
+
+            if(target.pq.size() >= 2){
+                answer.add(target.pq.poll().id);
+                answer.add(target.pq.poll().id);
+            }else{
+                answer.add(target.pq.poll().id);
+            }
+
+
+        }
+        return answer.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+    class Info implements Comparable<Info>{
+        int total;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        public Info(int total){
+            this.total = total;
+        }
+        @Override
+        public int compareTo(Info info){
+            return info.total - this.total;
+        }
+
+    }
+
+    class Node implements Comparable<Node>{
+        int id;
+        int num;
+
+        public Node(int id, int num){
+            this.id = id;
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Node node){
+            if(this.num == node.num){
+                return this.id - node.id; // id가 낮은 순으로
+            }
+            return node.num- this.num; // 재생 수가 많은 순으로
+        }
+    }
+}
