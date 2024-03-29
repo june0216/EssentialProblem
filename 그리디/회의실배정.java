7083f6d6323444aebb11ac47aa2dfffde3b692f6
kwonjiyun <ziyun1612@ewhain.net>
2024-03-29 16:47:36 +0900

    그리디 : 회의실 배정 - 배열

--- 그리디/회의실배정.java
+++ 그리디/회의실배정.java
@@ -3,46 +3,37 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 회의실배정 {
-    static class Node implements Comparable<Node>{
-        public int start;
-        public int end;
-        public int time; // 회의 시간
-        public Node(int start, int end){
-            this.start = start;
-            this.end = end;
-            this.time = end-start;
-        }
-        @Override
-        public int compareTo(Node node){
-            // 끝나는 시간 순으로 정렬한다.
-            if(this.end == node.end){ // 시간이 같으면 시작시간이 빠른 순으로 정렬한다. ->  최대 사용할 수 있는 회의의 최대 개수를 출력이므로 최대로 활용하기 위함
-                return this.start - node.start;
-            }
-            return this.end - node.end;
-        }
-    }
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+
         int total = Integer.parseInt(br.readLine());
+        int[][] meetings = new int[total][2];
         for(int i = 0 ; i < total; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            pq.offer(new Node(start, end));
-        }
+            meetings[i][0] = start;
+            meetings[i][1] = end;
 
-        int finish = pq.poll().end;
-        int cnt = 1;
-        while(!pq.isEmpty()){
-            Node node = pq.poll(); // 회의가 빨리 끝나는 회의 순서대로 꺼낸다.
-            if(finish <= node.start){ // 이전 회의가 끝난 이후에 시작하는 회의라면 회의가 가능하다 (회의가 끝난 동시에 시작 가능)
-                cnt++;
-                finish = node.end;
+        }
+        Arrays.sort(meetings, (s1, s2) -> {
+            if(s1[1] == s2[1]){
+                return s1[0] - s2[0]; // 시작 시간으로 정렬하기
             }
+            return s1[1] -s2[1]; // 끝나는 시간으로 정렬
+        });
 
+
+        int cnt = 1;
+        int finish = meetings[0][1];
+        for(int i = 1 ; i < total; i++){
+            if(meetings[i][0] >= finish){ // 이전 끝나는 시간보다 회의 시작 시간이 크거나 같으면 체크한다.
+                cnt++;
+                finish = meetings[i][1];
+            }
         }
+
         System.out.println(cnt);
     }
     public static void main(String[] args) throws Exception{
