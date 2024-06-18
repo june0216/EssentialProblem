359aae820808cecf2935afc4d4160c5024ff8f9e
kwonjiyun <ziyun1612@ewhain.net>
2024-06-18 15:52:41 +0900

    BFS : 험난한 등굣길(주석추가)

--- DFS&BFS/험난한등굣길.java
+++ DFS&BFS/험난한등굣길.java
@@ -45,14 +45,14 @@ public class 험난한등굣길 {
     static int[] dx = new int[]{0, 0, -1, 1};
     static int[] dy = new int[]{-1, 1, 0, 0};
 
-    int[][] diagonal = {{-1, 1},{1, 1},{1, -1},{-1, -1}};
+    int[][] diagonal = {{-1, 1},{1, 1},{1, -1},{-1, -1}}; //사각형의 네 모서리로, 각 모서리에서 다음 모서리로 이동할 때 사용
     public  void check(int y, int x, int d) {
         map[y][x] = -1;
         int ny = y;
         int nx = x - d;
 
-        for(int i=0; i<4; i++) {
-            for(int j=0; j<d; j++) {
+        for(int i=0; i<4; i++) { //diagonal 배열을 사용하여 네 방향 각각을 처리
+            for(int j=0; j<d; j++) { //각 방향에서 d번 만큼 점을 이동
                 nx += diagonal[i][1];
                 ny += diagonal[i][0];
                 if(!isRange(nx, ny)) continue;
