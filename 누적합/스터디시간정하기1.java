72acf3afefe66becfaf912f28c31f8a9bf19205c
kwonjiyun <ziyun1612@ewhain.net>
2024-07-03 15:49:17 +0900

    누적합 : 스터디시간 정하기1 - 주석추가

--- 누적합/스터디시간정하기1.java
+++ 누적합/스터디시간정하기1.java
@@ -27,6 +27,7 @@ public class 스터디시간정하기1 {
 
         for(int i = 1; i <= 100000; i++){ // 스위핑하기
             time[i] = time[i] + time[i-1];
+            
         }
 
 
@@ -40,10 +41,11 @@ public class 스터디시간정하기1 {
         int answerEnd = K-1;
         int max = sum[K-1];
         for(int i = 1 ;i <= 100000-K; i++){
+            // i 인덱스의 값이 의미하는 것은 ( i ~ i+1 ) 사이의 범위를 의미한다. 이에 따라 i ~ i+K 범위를 구하기 위해서는 end값이 sum 배열에서 (i+K-1) 인덱스는 i+K-1 ~ i+K 범위 이므로 이므로 해당 인덱스 값을 꺼내야 i+K 범위까지가 된다.
             if(sum[i+K-1] - sum[i-1] > max){ // 구간 합이 제일 큰 부분을 구하기
                 answerStart = i;
-                answerEnd = i+K;
-                max = sum[i+K-1] - sum[i-1];
+                answerEnd = i+K; // 끝 부분을 나타내는 i+K로 저장한다.
+                max = sum[i+K-1] - sum[i-1]; // i+K까지를 구하기 위해서는 i+K-1인덱스의 값을 꺼내야한다. 이는 i+K-1 ~ i+K 범위를 나타낸다.
             }
         }
 
