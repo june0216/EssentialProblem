33d16a41bb373e110359fc2e0b6afa0b96c4fa7d
kwonjiyun <ziyun1612@ewhain.net>
2024-07-18 13:49:17 +0900

    LIS : 가장 긴 증가하는 부분 수열 주석추가

--- LIS/가장긴증가하는부분수열.java
+++ LIS/가장긴증가하는부분수열.java
@@ -11,7 +11,7 @@ public class 가장긴증가하는부분수열 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] dp = new int[N+1];
+        int[] dp = new int[N+1]; // 해당 위치 idx에서 가질 수 있는 LIS
         int[] num = new int[N];
         for(int i = 0 ; i < N; i++){
             num[i] = Integer.parseInt(st.nextToken());
@@ -19,9 +19,9 @@ public class 가장긴증가하는부분수열 {
         dp[0] = 1;
         int max = 1;
         for(int i = 0 ; i < N; i++){
-            dp[i] = 1;
-            for(int j = 0; j < i; j++){
-                if(num[i] > num[j]){
+            dp[i] = 1; // 자기 자신은 일단 포함되므로
+            for(int j = 0; j < i; j++){ // 앞에서 보면서 자기가 합류할 애들을 모색함
+                if(num[i] > num[j]){ // 나보다 작으면 얘 다음에 내가 합류할 수 있는 가능성이 있으니까 최댓값 검사해보기
                     dp[i] = Math.max(dp[j]+1, dp[i]);
                 }
             }
